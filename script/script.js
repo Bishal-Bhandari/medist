55039ba3e06e11b348c8de94af380139dfd4e9b1
Bishal Bhandari <bishalbhaandari62@gmail.com>
2019-07-07 13:20:27 +0545

    Maintain

--- script/script.js
+++ script/script.js
@@ -4,10 +4,12 @@ $(document).ready(function () {
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     if (scroll > 80) {
-      $(".navbar").css("background", "#fbeaea");
+      $(".navbar").css("background", "#fff");
+      $(".navbar").css("box-shadow", "rgb(195, 193, 193) 0px 0px 15px");
     }
     else {
       $(".navbar").css("background", "none");
+      $(".navbar").css("box-shadow", "none");
     }
   })
 })
