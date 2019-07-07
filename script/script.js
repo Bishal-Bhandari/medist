ab0321c80630322868c6684a7414f8ba5bb0c7d8
Bishal Bhandari <bishalbhaandari62@gmail.com>
2019-07-07 12:15:04 +0545

    layout fixed

--- script/script.js
+++ script/script.js
@@ -5,12 +5,9 @@ $(document).ready(function () {
     var scroll = $(window).scrollTop();
     if (scroll > 80) {
       $(".navbar").css("background", "#fbeaea");
-      // $(".navbar").css("color", " ");
-      // $(".Navbar").css("width", "94%");
     }
     else {
       $(".navbar").css("background", "none");
-      // $(".navbar").css("color", "#638bff");
     }
   })
 })
