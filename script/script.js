49649a30473a04820c2da5b1a1f1042aa835e66d
Bishal Bhandari <bishalbhaandari62@gmail.com>
2019-07-11 16:09:19 +0545

    upaded

--- script/script.js
+++ script/script.js
@@ -5,11 +5,13 @@ $(document).ready(function () {
     var scroll = $(window).scrollTop();
     if (scroll > 80) {
       $(".navbar").css("background", "#fff");
+      $(".nav-link-strong").css("color", "rgb(7, 53, 102)");
       $(".navbar").css("box-shadow", "rgb(195, 193, 193) 0px 0px 15px");
     }
     else {
       $(".navbar").css("background", "none");
       $(".navbar").css("box-shadow", "none");
+      $(".nav-link-strong").css("color", "rgb(255, 255, 255)");
     }
   })
 })
