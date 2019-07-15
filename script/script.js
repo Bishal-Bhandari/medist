828fa3e73cf927e825fbeb26fb1f63f0f044ca59
Bishal Bhandari <bishalbhaandari62@gmail.com>
2019-07-15 13:06:36 +0545

    js to change navbar color for diff fixed

--- script/script.js
+++ script/script.js
@@ -3,16 +3,30 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll > 80) {
-      $(".navbar").css("background", "#fff");
-      $(".nav-link-strong").css("color", "rgb(7, 53, 102)");
-      $(".navbar").css("box-shadow", "rgb(195, 193, 193) 0px 0px 15px");
-    }
-    else {
-      $(".navbar").css("background", "none");
-      $(".navbar").css("box-shadow", "none");
-      $(".nav-link-strong").css("color", "rgb(255, 255, 255)");
-    }
+     if($(window).width() > 768){
+        if (scroll > 80) {
+          $(".navbar").css("background", "#fff");
+          $(".nav-link-strong").css("color", "rgb(7, 53, 102)");
+          $(".navbar").css("box-shadow", "rgb(195, 193, 193) 0px 0px 15px");
+        }
+
+        else {
+          $(".navbar").css("background", "none");
+          $(".navbar").css("box-shadow", "none");
+          $(".nav-link-strong").css("color", "rgb(255, 255, 255)");
+        }}
+      else{
+        if (scroll > 80) {
+          $(".navbar").css("background", "#fff");
+          $(".nav-link-strong").css("color", "rgb(7, 53, 102)");
+          $(".navbar").css("box-shadow", "rgb(195, 193, 193) 0px 0px 15px");
+        }
+
+        else {
+          $(".navbar").css("background", "gray");
+          $(".navbar").css("box-shadow", "none");
+          $(".nav-link-strong").css("color", "rgb(255, 255, 255)");
+        }}
   })
 })
 
