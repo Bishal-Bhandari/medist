0ea48659242f54645fd262d998f0d73b876585ec
Bishal Bhandari <bishalbhaandari62@gmail.com>
2019-07-15 14:55:31 +0545

    js to change navbar color for diff fixed

--- script/changeLogo.js
+++ script/changeLogo.js
@@ -1,12 +1,15 @@
-// change logo on scroll
-$(function () { 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 80) { 
-            $('.navbar .navbar-brand img').attr('src','img/brand-dark.png');
+//// to show background color on scroll
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+     if($(window).width() > 768){
+        if (scroll > 80) {
+          $('.navbar .navbar-brand img').attr('src','img/brand-dark.png');
         }
-        if ($(this).scrollTop() < 80) { 
-            $('.navbar .navbar-brand img').attr('src','img/brand-light.png');
-        }
-    })
-  });
-  
+
+        else {
+          $('.navbar .navbar-brand img').attr('src','img/brand-light.png');
+        }}
+  })
+})
+//   
